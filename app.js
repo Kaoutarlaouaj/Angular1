7e06984603ac7d8ed489b85eb87fb0b624f0771a
kawtar <kaoutar.laouaj@ump.ac.ma>
2024-01-29 17:42:28 +0100

    Next version of web file

--- app.js
+++ app.js
@@ -6,7 +6,10 @@
 angular.module('myFirstApp', [])
 
 .controller('MyFirstController',function ($scope) {
- $scope.name = "Kaoutar";
+    $scope.name = "Kaoutar";
+    $scope.sayHello = function () {
+        return "Hello coursera";
+    };
 });
 
 })();
